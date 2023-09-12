3f992f6d23e404796b1149a1cc16ea9eed968b8c
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-12 23:41:46 +0200

    Add project files.

---
+++ Switch Statements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 3");
+            int number = int.Parse(Console.ReadLine()); //convert to an int
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("you entered 1");
+                    break;
+                case 2:
+                    Console.WriteLine("you entered 2");
+                    break;
+                case 3:
+                    Console.WriteLine("you entered 3 ");
+                    break;
+                default: Console.WriteLine("you broke rules");
+                    break;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
